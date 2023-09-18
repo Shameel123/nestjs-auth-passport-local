16a7fb408222ba8afe3ff201350f1614ce856f54
Shameel Uddin <shameel_uddin@yahoo.com>
2023-09-18 15:49:12 +0500

    user and auth modules created and linked.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      username: 'jane',
+      password: 'secret',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
