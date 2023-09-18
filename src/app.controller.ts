a5150af9fb0ccca3a1e68fa894643d9b720df6b0
Shameel Uddin <shameel_uddin@yahoo.com>
2023-09-18 16:52:44 +0500

    jwt auth completed.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,6 +9,7 @@ import {
 import { User } from './users/users.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -17,13 +18,12 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Request() req): Promise<any> {
-    // TODO: return JWT access token
     return this.authService.login(req.user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/profile')
   getProfile(@Request() req): any {
-    // TODO: Require Bearer Token and Validate Token
     return req.user;
   }
 }
